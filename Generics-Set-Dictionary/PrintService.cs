ae3a63ead773cb89bb06b133735c0b406baea60d
Matheus Moreira <matheus.moreira127@gmail.com>
2025-09-05 13:44:37 -0300

    Add project files.

---
+++ Generics-Set-Dictionary/PrintService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Set_Dictionary
+{
+    internal class PrintService
+    {
+        private int[] _values = new int[10]; // Vetor de inteiros com tamanho 10
+        private int _count = 0; // conta quantos elementos foram adicionados
+
+        public void AddValue(int value)
+        {
+            if (_count == 10) // Verifica se o vetor está cheio
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+
+            _values[_count] = value; // Adiciona o valor no vetor
+            _count++; // Incrementa o contador
+        }
+
+        public int First()
+        {
+            if (_count == 0) // Verifica se o vetor está cheio
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count; i++) // Imprime até a penultima posição do vetor
+            {
+                Console.Write(_values[i] + ", " );
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]); // Imprime o último elemento sem a vírgula
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Generics-Set-Dictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using Generics_Set_Dictionary;
+
+PrintService printService = new PrintService();
+
+Console.WriteLine("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);
+}
+
+printService.Print();
+Console.WriteLine("First: " + printService.First());
