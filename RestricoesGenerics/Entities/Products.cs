990990293c10c05b3ca3d2d778837c12e8fb2871
Matheus Moreira <matheus.moreira127@gmail.com>
2025-09-09 13:46:57 -0300

    Iniciando a utilização de Restrição Generics

---
+++ RestricoesGenerics/Entities/Products.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System.Globalization;
+
+namespace RestricoesGenerics.Entities
+{
+    internal class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Products(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Price.ToString("F2");
+        }
+    }
+}
---
+++ RestricoesGenerics/Program.cs
@@ -0,0 +1,20 @@
+﻿using RestricoesGenerics.Entities.Services;
+using RestricoesGenerics.Entities;
+
+List<Products> list = new List<Products>();
+
+Console.WriteLine("Enter N: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] vect = Console.ReadLine().Split(',');
+    int x = int.Parse(Console.ReadLine());
+    list.Add(x);
+}
+
+CauculationService cs = new CauculationService();
+int max = cs.Max(list);
+
+Console.WriteLine("Max: ");
+Console.WriteLine(max); 
