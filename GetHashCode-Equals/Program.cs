710606fd8b22baf7ca773eebfc31686c1485e825
Matheus Moreira <matheus.moreira127@gmail.com>
2025-09-15 13:37:15 -0300

    Incrementando o uso de GetHashCode e Equals

---
+++ GetHashCode-Equals/Program.cs
@@ -0,0 +1,8 @@
+﻿using GetHashCode_Equals.Entities;
+
+Client a = new Client{Name = "Maria", Email = "maria@hmail.com"};
+Client b = new Client{Name = "Alex", Email = "alex@gmail.com"};
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
