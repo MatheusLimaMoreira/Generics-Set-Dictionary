bd5453851a92f5b12a6e0eff3d0d0e90a9b90360
Matheus Moreira <matheus.moreira127@gmail.com>
2025-09-29 13:28:56 -0300

    iniciando aplicação de HashSet e SortedSet

---
+++ HashSetSortedSet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+HashSet<string> set = new HashSet<string>();
+
+set.Add("TV");
+set.Add("Radio");
+set.Add("Phone");
+
+Console.WriteLine(set.Contains("Phone"));
+
+foreach (string p in set)
+{
+    Console.WriteLine(p);
+}
